f19762fdb701c13b1ea6acab444ac2bb76dc59ba
Himanshu-VW <himanshuyadaav560@gmail.com>
2026-07-09 06:09:45 +0530

    Create dictionary.py

---
+++ data_structures/hash_tables/dictionary.py
@@ -0,0 +1,6 @@
+#using 
+from collections import defaultdict
+
+default = defaultdict(int)
+'''it initializes any element not present as key with 0 so we don't get a key error
+without actually initializing manually we can just do out operation on it like append or in case of int + or -'''
